d4f0bacaf8cc93fca84b9cec2a51f50b61d4c96c
merzlonog <yaroslav-skachkov@mail.ru>
2024-01-22 21:22:29 +0300

    add ui components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,16 +7,16 @@ import { useTheme } from "app/providers/ThemeProvider";
 
 import "./styles/index.scss";
 import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <div className={classNames("app", {}, [theme])}>
-      <button onClick={toggleTheme}>SWITCH THEME</button>
-      <Link to={"/"}>Главная</Link>
-      <Link to={"/about"}>О сайте</Link>
+      <Navbar />
       <AppRouter />
+      <button onClick={toggleTheme}>SWITCH THEME</button>
     </div>
   );
 };
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,27 @@
+import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+import { classNames } from "shared/lib/classNames/classNames";
+
+import style from "./Navbar.module.scss";
+
+interface NavbarProps {
+  className?: string;
+}
+
+export const Navbar = ({ className }: NavbarProps) => {
+  return (
+    <div className={classNames(style.navbar, {}, [className])}>
+      <div className={style.links}>
+        <AppLink
+          theme={AppLinkTheme.SECONDARY}
+          to={"/"}
+          className={style.mainLink}
+        >
+          Главная
+        </AppLink>
+        <AppLink theme={AppLinkTheme.RED} to={"/about"}>
+          О сайте
+        </AppLink>
+      </div>
+    </div>
+  );
+};
